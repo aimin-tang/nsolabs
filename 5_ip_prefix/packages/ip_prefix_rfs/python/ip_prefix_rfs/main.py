9acc3e33e30ee96b85e9da501d25281b462c5206
Aimin Tang <aimin.tang@gmail.com>
2020-12-27 17:34:57 -0600

    use template

--- 5_ip_prefix/packages/ip_prefix_rfs/python/ip_prefix_rfs/main.py
+++ 5_ip_prefix/packages/ip_prefix_rfs/python/ip_prefix_rfs/main.py
@@ -14,22 +14,51 @@ class ServiceCallbacks(Service):
         return name, desc, device, entries
 
     def get_entry_leaves(self, entry):
-        return entry.subnet, entry.op.prefix_length_op, entry.op.prefix_length
+        # all in str format
+        return str(entry.sm_verb), entry.subnet_mask, str(entry.op_verb), entry.op_length
 
-    def create_prefix(self, prefixes, name, desc):
-        prefix = prefixes.create(name)
-
+    def create_prefix(self, prefixes, ip_prefix_name, desc, entries):
+        prefix = prefixes.create(ip_prefix_name)
+        prefix.description = desc
+        for i, entry in enumerate(entries):
+            sm_verb, subnet_mask, op_verb, op_length = self.get_entry_leaves(entry)
+            print("========")
+            print(sm_verb, type(sm_verb))
+            print(subnet_mask, type(subnet_mask))
+            print(op_verb, type(op_verb))
+            print(op_length, type(op_length))
+            print("--------")
+            seq = (i + 1) * 5
+            prefix.seq.create(seq)
+            if sm_verb == 'permit':
+                prefix.seq[seq].permit = subnet_mask
+            elif sm_verb == 'deny':
+                prefix.seq[seq].deny = subnet_mask
+            if op_verb == 'le':
+                prefix.seq[seq].le = op_length
+            elif op_verb == 'gt':
+                prefix.seq[seq].gt = op_length
+            elif op_verb == 'eq':
+                prefix.seq[seq].eq = op_length
 
     @Service.create
     def cb_create(self, tctx, root, service, proplist):
         self.log.info('Service create(service=', service._path, ')')
-        with ncs.maapi.single_write_trans('admin', 'python') as t:
-            root = ncs.maagic.get_root(t)
-            # name, desc, device, entries = self.get_service_params(service)
-            # device = root.devices.device[device]
-            # prefixes = device.config.nx__ip.prefixes
-            # self.create_prefix(prefixes, name, desc)
+        # with ncs.maapi.Maapi() as m:
+        #     with ncs.maapi.Session(m, 'admin', 'python'):
+        #         with m.start_write_trans() as t:
+        #             ip_prefix_name, desc, device_name, entries = self.get_service_params(service)
+        #             device = root.devices.device[device_name]
+        #             prefixes = device.config.nx__ip.prefix_list.prefixes
+        #             self.create_prefix(prefixes, ip_prefix_name, desc, entries)
+        #             t.apply()
 
+        ip_prefix_name, desc, device_name, entries = self.get_service_params(service)
+        vars = ncs.template.Variables()
+        vars.add('IP_PREFIX_NAME', ip_prefix_name)
+        vars.add('DESC', desc)
+        template = ncs.template.Template(service)
+        template.apply('ip_prefix_rfs-template', vars)
 
     # @Service.pre_modification
     # def cb_pre_modification(self, tctx, op, kp, root, proplist):
--- 5_ip_prefix/packages/ip_prefix_rfs/python/ip_prefix_rfs/tmp.py
+++ 5_ip_prefix/packages/ip_prefix_rfs/python/ip_prefix_rfs/tmp.py
@@ -1,29 +1,45 @@
 import ncs
 
+def get_entry_leaves(entry):
+    return 'permit', '1.1.1.1/32', 'le', '32'
 
-def parse_service():
-    with ncs.maapi.single_write_trans('admin', 'python') as t:
-        root = ncs.maagic.get_root(t)
-        service = root.ip_prefix_rfs__ip_prefix_rfs['l1']
-        name = service.list_name
-        desc = service.description
-        device = service.device
-        entries = service.prefix_entry
-
-    return name, desc, device, entries
-
+def create_prefix(prefixes, ip_prefix_name, desc, entries):
+    prefix = prefixes.create(ip_prefix_name)
+    prefix.description = desc
+    for i, entry in enumerate(entries):
+        sm_verb, subnet_mask, op_verb, op_length = get_entry_leaves(entry)
+        print("========")
+        print(sm_verb, type(sm_verb))
+        print(subnet_mask, type(subnet_mask))
+        print(op_verb, type(op_verb))
+        print(op_length, type(op_length))
+        print("--------")
+        seq = (i + 1) * 5
+        prefix.seq.create(seq)
+        if str(sm_verb) == 'permit':
+            prefix.seq[seq].permit = subnet_mask
+        elif str(sm_verb) == 'deny':
+            prefix.seq[seq].deny = subnet_mask
+        if op_verb == 'le':
+            prefix.seq[seq].le = op_length
+        elif op_verb == 'gt':
+            prefix.seq[seq].gt = op_length
+        elif op_verb == 'eq':
+            prefix.seq[seq].eq = op_length
 
 def device_config():
-    with ncs.maapi.single_write_trans('admin', 'python') as t:
-        root = ncs.maagic.get_root(t)
-        device = root.devices.device['c0']
-        prefixes = device.config.nx__ip.prefix_list.prefixes
-        prefix = prefixes.create('l111')
-        prefix.description = 'd111'
-        seq = prefix.seq.create(5)
-        print(dir(seq))
+    with ncs.maapi.Maapi() as m:
+        with ncs.maapi.Session(m, 'admin', 'python'):
+            with m.start_write_trans() as t:
+                root = ncs.maagic.get_root(t)
+                device = root.devices.device['c0']
+                # device.config.nx__banner.exec.start_marker = 'exec2'
+                prefixes = device.config.nx__ip.prefix_list.prefixes
+                create_prefix(prefixes, 'l111', 'd111', [0])
+                t.apply()
 
 
 if __name__ == '__main__':
-    parse_service()
-    # device_config()
+    # parse_service()
+    device_config()
+    # ruiying()
